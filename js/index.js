86a75ac7f959ef32e92d328b9a7f565fcc8f08f4
Dickson <hhhh820605@gmail.com>
2021-12-29 21:53:20 +0800

    buttom function

--- js/index.js
+++ js/index.js
@@ -41,31 +41,34 @@ function getLottoNumber(start, end, numbers, special = false) {
     return lotto;
 }
 
+
+
+const lottoEl = document.querySelector('.lotto_number');
 const startBtn = document.querySelector('#start');
-startBtn.addEventListener('click', () => {
-    console.log('click');
-})
 const title = document.querySelector('h1');
-title.innerText = '2021/12/29大樂透號碼';
 const subtitle = document.querySelector('h3');
-subtitle.innerHTML = '<h2>本期大樂透即將開獎!請按鈕開始</h2>';
-
-
 const start = 1;
 const end = 49;
-lottoEl = '<ul >';
-for (let a = 0; a < 5; a++) {
-    let lotto = '';
-    let num = getLottoNumber(start, end, 6, true);
-    //let lotto = (`第${i + 1}個號碼:${num}`);
 
-    lotto += num + ' &nbsp;&nbsp;';
-    lottoEl += `<li>${lotto}</li>`;
-}
 
-lottoEl += '</ul>';
+startBtn.addEventListener('click', () => {
+    console.log('click');
+    lottoEl.innerHTML = '<ul >';
+    for (let a = 0; a < 5; a++) {
+        let lotto = '';
+        let num = getLottoNumber(start, end, 6, true);
+        //let lotto = (`第${i + 1}個號碼:${num}`);
+
+        lotto += num + ' &nbsp;&nbsp;';
+        lottoEl.innerHTML += `<li>${lotto}</li>`;
+    }
+    lottoEl.innerHTML += '</ul>';
+})
+
+title.innerText = '2021/12/29大樂透號碼';
+subtitle.innerHTML = '<h2>本期大樂透即將開獎!請按鈕開始</h2>';
 console.log(lottoEl);
-document.write(lottoEl);
+//document.write(lottoEl);
 
 
 
